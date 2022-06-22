dae037c2e7623e781efabaccb0c5f3f922063dc8
Mikey <mikey@mikeycorporation.com>
2022-06-22 22:21:50 +0100

    Support server

--- erm.py
+++ erm.py
@@ -235,6 +235,7 @@ async def update_bot_status():
 		embed.set_thumbnail(url = bot.user.avatar.url)
 		embed.add_field(name = 'Last ping', value = f'<t:{int(datetime.datetime.now().timestamp())}:R>')
 		embed.add_field(name = 'Status', value = '<:online:989218581764014161> Online')
+		embed.add_field(name = 'Pings', value = '1')
 		embed.add_field(name = 'Note', value = f'This is updated every 30 seconds. If you see the last ping was over 30 seconds ago, contact {discord.utils.get(channel.guild.members, id = 635119023918415874).mention}', inline = False)
 
 		await channel.send(embed = embed)
@@ -242,6 +243,7 @@ async def update_bot_status():
 		last_embed = last_message.embeds[0]
 		last_embed_fields = last_embed.fields
 		last_embed_fields[0].value = f'<t:{int(datetime.datetime.now().timestamp())}:R>'
+		last_embed_fields[1].value = str(int(last_embed_fields[1].value) + 1)
 		last_embed.fields = last_embed_fields
 
 		await last_message.edit(embed=last_embed)
