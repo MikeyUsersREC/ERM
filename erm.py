921de76196723dbf45579926b254462d5ef6d1ee
Mikey <mikey@mikeycorporation.com>
2022-06-22 22:00:37 +0100

    Setting up logging

--- erm.py
+++ erm.py
@@ -233,9 +233,9 @@ async def update_bot_status():
 			color = discord.Color.red()
 		)
 		embed.set_thumbnail(url = bot.user.avatar.url)
-		embed.add_field(name = 'Last ping', value = f'<t:{datetime.datetime.now().timestamp()}:R>')
+		embed.add_field(name = 'Last ping', value = f'<t:{int(datetime.datetime.now().timestamp())}:R>')
 		embed.add_field(name = 'Status', value = '<:online:989218581764014161> Online')
-		embed.add_field(name = 'Note', value = f'This is updated every 30 seconds. If you see the last ping was over 30 seconds ago, contact {discord.utils.get(channel.guild.members, id = 635119023918415874).mention}')
+		embed.add_field(name = 'Note', value = f'This is updated every 30 seconds. If you see the last ping was over 30 seconds ago, contact {discord.utils.get(channel.guild.members, id = 635119023918415874).mention}', inline = False)
 
 		await channel.send(embed = embed)
 	else:
