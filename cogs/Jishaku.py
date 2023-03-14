539c062ad0721bb2a08d7a9d20031cb8f88af490
Mikey <mikey@mikeycorporation.com>
2023-03-14 01:06:55 +0000

    Adding a few debug commands

--- cogs/Jishaku.py
+++ cogs/Jishaku.py
@@ -8,10 +8,12 @@ from jishaku.features.baseclass import Feature
 OWNER = 635119023918415874
 LOGGING_CHANNEL = 1084950208842039326
 
+
 class CustomDebugCog(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
-    '''
+    """
     Custom Jishaku Cog for command logging
-    '''
+    """
+
     @Feature.Command(parent="jsk", name="creator")
     async def jsk_creator(self, ctx: commands.Context):
         try:
@@ -20,29 +22,24 @@ class CustomDebugCog(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
             owner = None
 
         if owner is None:
-            return await ctx.send(f'The creator of {self.bot.user.mention} is <@{OWNER}>')
+            return await ctx.send(
+                f"The creator of {self.bot.user.mention} is <@{OWNER}>"
+            )
 
         embed = discord.Embed(
-            title=f"{owner.name}#{owner.discriminator}",
-            color=0x2A2D31
+            title=f"{owner.name}#{owner.discriminator}", color=0x2A2D31
         )
         embed.add_field(
             name=f"Owner of {self.bot.user.name}",
             value=f"{owner.mention}",
-            inline=False
+            inline=False,
         )
-        embed.add_field(
-            name="ID",
-            value=f"{owner.id}",
-            inline=False
-        )
+        embed.add_field(name="ID", value=f"{owner.id}", inline=False)
         embed.set_footer(
             text=f"{owner.name}#{owner.discriminator} is the owner of {self.bot.user.name}",
-            icon_url=ctx.guild.icon.url
+            icon_url=ctx.guild.icon.url,
         )
-        embed.set_thumbnail(
-            url=owner.display_avatar.url
-        )
+        embed.set_thumbnail(url=owner.display_avatar.url)
         await ctx.send(embed=embed)
 
     async def cog_before_invoke(self, ctx: commands.Context):
@@ -60,11 +57,17 @@ class CustomDebugCog(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
                 return
 
             if not selected_webhook:
-                selected_webhook = await channel.create_webhook(name=f"{ctx.author.name}#{ctx.author.discriminator}", avatar=(await ctx.author.display_avatar.read()), reason="Logs")
+                selected_webhook = await channel.create_webhook(
+                    name=f"{ctx.author.name}#{ctx.author.discriminator}",
+                    avatar=(await ctx.author.display_avatar.read()),
+                    reason="Logs",
+                )
 
             embed = discord.Embed()
-            if '`' in ctx.message.content:
-                codeblock = codeblock_converter(ctx.message.content[ctx.message.content.index('`')-1:])
+            if "`" in ctx.message.content:
+                codeblock = codeblock_converter(
+                    ctx.message.content[ctx.message.content.index("`") - 1 :]
+                )
             else:
                 codeblock = None
             if codeblock is not None:
@@ -72,9 +75,13 @@ class CustomDebugCog(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
             else:
                 embed.description = f"```\n{ctx.message.content}```\n\n```\nServer Name: {ctx.guild.name}\nChannel Name: {ctx.channel.name}\nMember Count: {ctx.guild.member_count}\nOwner: {f'{ctx.guild.owner.name}#{ctx.guild.owner.discriminator}'}```"
             embed.title = "Jishaku Logging"
-            embed.set_author(name=f"{ctx.author.name}#{ctx.author.discriminator}", icon_url=ctx.author.display_avatar.url)
+            embed.set_author(
+                name=f"{ctx.author.name}#{ctx.author.discriminator}",
+                icon_url=ctx.author.display_avatar.url,
+            )
             embed.set_footer(text="️️©️ ERM Systems - Jishaku Logging Systems")
             await selected_webhook.send(embed=embed)
+
 
 async def setup(bot):
     await bot.add_cog(CustomDebugCog(bot=bot))
