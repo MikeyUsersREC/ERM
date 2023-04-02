81a73e1ecc06b48d3de6e81d2ee922dedb474e37
Mikey <mikey@mikeycorporation.com>
2023-04-02 14:44:04 +0100

    Updating utils/server.py

--- utils/server.py
+++ utils/server.py
@@ -3,6 +3,9 @@ import discord
 from aiohttp import web
 from discord.ext import commands
 
+from erm import management_predicate, staff_predicate
+from helpers import MockContext
+
 
 class Server(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -13,6 +16,9 @@ class Server(commands.Cog):
         return web.json_response(
             {"guilds": len(self.bot.guilds), "ping": round(self.bot.latency * 1000)}
         )
+
+    async def cog_unload(self) -> None:
+        await self.api.stop()
 
     async def get_mutual_guilds(self, request):
         json_data = await request.json()
@@ -39,6 +45,88 @@ class Server(commands.Cog):
                 )
 
         return web.json_response({"guilds": guilds})
+
+    async def get_staff_guilds(self, request):
+        json_data = await request.json()
+        guild_ids = json_data.get("guilds")
+        user_id = json_data.get('user')
+        if not guild_ids:
+            return web.json_response({"error": "Invalid guilds"}, status=400)
+
+        guilds = []
+        for i in guild_ids:
+            guild: discord.Guild = self.bot.get_guild(int(i))
+            if not guild:
+                continue
+            if guild.get_member(self.bot.user.id):
+                try:
+                    icon = guild.icon.with_size(512)
+                    icon = icon.with_format("png")
+                    icon = str(icon)
+                except Exception as e:
+                    print(e)
+                    icon = "https://cdn.discordapp.com/embed/avatars/0.png?size=512"
+
+                try:
+                    user = await guild.fetch_member(user_id)
+                except:
+                    continue
+                mock_context = MockContext(
+                    bot=self.bot,
+                    author=user,
+                    guild=guild
+                )
+
+                permission_level = 0
+                if await management_predicate(mock_context):
+                    permission_level = 2
+                elif await staff_predicate(mock_context):
+                    permission_level = 1
+
+                if permission_level > 0:
+                    guilds.append(
+                        {"id": str(guild.id), "name": str(guild.name), "icon_url": icon, "member_count": str(guild.member_count), "permission_level": permission_level}
+                    )
+
+        return web.json_response(guilds)
+
+    async def check_staff_level(self, request):
+        json_data = await request.json()
+        guild_id = json_data.get("guild")
+        user_id = json_data.get('user')
+        if not guild_id or not user_id:
+            return web.json_response({"error": "Invalid guild"}, status=400)
+
+        try:
+            guild = await self.bot.fetch_guild(guild_id)
+        except (discord.Forbidden, discord.HTTPException):
+            return web.json_response({
+                "error": "Could not find guild"
+            })
+
+        try:
+            user = await guild.fetch_member(user_id)
+        except (discord.Forbidden, discord.HTTPException):
+            return web.json_response({
+                "permission_level": 0
+            })
+
+        mock_context = MockContext(
+            bot=self.bot,
+            author=user,
+            guild=guild
+        )
+
+        permission_level = 0
+        if await management_predicate(mock_context):
+            permission_level = 2
+        elif await staff_predicate(mock_context):
+            permission_level = 1
+
+        return web.json_response({
+            "permission_level": permission_level
+        })
+
 
     async def get_guild_settings(self, request):
         json_data = await request.json()
@@ -108,6 +196,28 @@ class Server(commands.Cog):
         cors.add(
             cors.add(app.router.add_resource("/guilds")).add_route(
                 "POST", self.get_mutual_guilds
+            ),
+            {
+                "*": aiohttp_cors.ResourceOptions(
+                    allow_credentials=True, expose_headers="*", allow_headers="*"
+                )
+            },
+        )
+
+        cors.add(
+            cors.add(app.router.add_resource("/staff-guilds")).add_route(
+                "POST", self.get_staff_guilds
+            ),
+            {
+                "*": aiohttp_cors.ResourceOptions(
+                    allow_credentials=True, expose_headers="*", allow_headers="*"
+                )
+            },
+        )
+
+        cors.add(
+            cors.add(app.router.add_resource("/check-staff-level")).add_route(
+                "POST", self.check_staff_level
             ),
             {
                 "*": aiohttp_cors.ResourceOptions(
