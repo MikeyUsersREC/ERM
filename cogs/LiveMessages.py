c19c0314fe91477ab51ba851c302ce6c5953c54c
Mikey <mikey@ermbot.xyz>
2023-08-07 03:26:40 +0100

    fixing bolo active

--- cogs/LiveMessages.py
+++ cogs/LiveMessages.py
@@ -9,19 +9,19 @@ class LiveMessages(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_group("livemessage")
-    async def livemessage(self, ctx):
-        pass
-
-    @livemessage.command(
-        name="panel",
-        description="Sends a panel to a channel so that staff members can use ERM features easily",
-        extras={"category": "Live Message"},
-    )
-    async def livemessage_panel(
-        self, ctx: commands.Context, channel: discord.TextChannel
-    ):
-        await ctx.send(view=LiveMenu(self.bot, ctx))
+    # @commands.hybrid_group("livemessage")
+    # async def livemessage(self, ctx):
+    #     pass
+    #
+    # @livemessage.command(
+    #     name="panel",
+    #     description="Sends a panel to a channel so that staff members can use ERM features easily",
+    #     extras={"category": "Live Message"},
+    # )
+    # async def livemessage_panel(
+    #     self, ctx: commands.Context, channel: discord.TextChannel
+    # ):
+    #     await ctx.send(view=LiveMenu(self.bot, ctx))
 
 
 async def setup(bot):
