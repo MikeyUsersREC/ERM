84abf148b117c8323f2e2a935cc788fcc1d64175
shashankpandey04 <jpshashank200@gmail.com>
2024-05-24 18:36:47 +0530

    Fixed Remote Command Logs Issue

--- events/on_message.py
+++ events/on_message.py
@@ -35,13 +35,14 @@ class OnMessage(commands.Cog):
         if not message.guild:
             return
 
-        if isinstance(message.author, discord.User):
+        #Embeds are send as discord.User identity, so if we return it then it will not be able to fetch the content of the message
+        #if isinstance(message.author, discord.User):
             #msg = copy.copy(message)
             #msg.author = await message.guild.fetch_member(message.author.id)
             #message = msg
             #ch = await bot.fetch_channel(1213731821330894938)
             #await ch.send(f"{type(message.author)=} | {message.guild.chunked=} | {message.guild.member_count=} | {len(message.guild.members)=}")
-            return
+            #return
         
         dataset = await bot.settings.find_by_id(message.guild.id)
         if dataset == None:
