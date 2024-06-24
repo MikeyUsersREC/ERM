fa2fbb9ebe0d290e77b166c0f9ec8143042e7b29
Noah <117038300+NoahCxrest@users.noreply.github.com>
2024-06-24 17:18:19 -0600

    Update erm.py

--- erm.py
+++ erm.py
@@ -226,7 +226,6 @@ bot = Bot(
     command_prefix=get_prefix,
     case_insensitive=True,
     intents=intents,
-    chunk_guilds_at_startup=False,
     help_command=None,
     allowed_mentions=discord.AllowedMentions(
         replied_user=False, everyone=False, roles=False
