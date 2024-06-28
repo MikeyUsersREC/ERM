019b1201949e7149ba4c7f383d6c663d794ec61d
i_iMikey <67828573+MikeyUsersREC@users.noreply.github.com>
2024-06-28 18:27:10 +0100

    Update Punishments.py

--- cogs/Punishments.py
+++ cogs/Punishments.py
@@ -63,7 +63,6 @@ class Punishments(commands.Cog):
     )
     @app_commands.describe(reason="What is your reason for punishing this user?")
     async def punish(self, ctx, user: str, type: str, *, reason: str):
-        await ctx.typing()
         settings = await self.bot.settings.find_by_id(ctx.guild.id) or {}
         if not settings:
             return await ctx.send(
