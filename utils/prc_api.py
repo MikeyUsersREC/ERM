84e0af9f516f35545aac7ac5fc407ba816fa651a
Shashank Pandey <jpshashank200@gmail.com>
2024-08-30 09:59:48 +0530

    Updated prc_api.py

--- utils/prc_api.py
+++ utils/prc_api.py
@@ -128,7 +128,8 @@ class PRCApiClient:
             #     })
             #     response.status = 423
             if response.status == 429:
-                retry_after = int(response.headers.get('X_RateLimit_Reset'+1, 5))
+                X_RateLimit_Reset = int(response.headers.get('X-RateLimit-Reset', 0))
+                retry_after = X_RateLimit_Reset - int(datetime.datetime.now().timestamp())
                 await asyncio.sleep(retry_after)
                 return await self._send_api_request(method, endpoint, guild_id, data, key)
 
