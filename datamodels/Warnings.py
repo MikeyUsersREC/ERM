ab46a9ef5d404293eca2488921bdb837b33f01e8
Noah <117038300+NoahCxrest@users.noreply.github.com>
2024-10-09 18:37:05 -0600

    Update Warnings.py

--- datamodels/Warnings.py
+++ datamodels/Warnings.py
@@ -130,80 +130,73 @@ class Warnings(Document):
         ]
 
     async def insert_warning(
-            self,
-            staff_id: int,
-            staff_name: str,
-            user_id: int,
-            user_name: str,
-            guild_id: int,
-            reason: str,
-            moderation_type: str,
-            time_epoch: int,
-            until_epoch: int | None = None,
-    ) -> ObjectId | ValueError:
-        """
-        Inserts a warning into the database.
+        self,
+        staff_id: int,
+        staff_name: str,
+        user_id: int,
+        user_name: str,
+        guild_id: int,
+        reason: str,
+        moderation_type: str,
+        time_epoch: int,
+        until_epoch: int | None = None,
+) -> ObjectId | ValueError:
+    if all([until_epoch is None, moderation_type == "Temporary Ban"]):
+        return ValueError("Epoch must be provided for temporary bans.")
+
+    if any(
+            not i
+            for i in [
+                staff_id,
+                staff_name,
+                user_id,
+                user_name,
+                guild_id,
+                reason,
+                moderation_type,
+            ]
+    ):
+        return ValueError("All arguments must be provided.")
+
+    identifier = ObjectId()
+
+    await self.db.insert_one(
         {
-          "_id": 123456789012345678,
-          "Username": "1friendlydoge",
-          "UserID": 123456789012345678,
-          "Type": "Warning",
-          "Reason": "Nerd",
-          "Moderator": "Noah",
-          "ModeratorID": 123456789012345678,
-          "Guild": 12345678910111213,
-          "Epoch": 706969420,
-          "UntilEpoch": 706969420
+            "_id": identifier,
+            "Snowflake": next(generator),
+            "Username": user_name,
+            "UserID": user_id,
+            "Type": moderation_type,
+            "Reason": reason,
+            "Moderator": staff_name,
+            "ModeratorID": staff_id,
+            "Guild": guild_id,
+            "Epoch": int(time_epoch),
+            "UntilEpoch": int(until_epoch if until_epoch is not None else 0),
         }
-        """
-        if all([until_epoch is None, moderation_type == "Temporary Ban"]):
-            return ValueError("Epoch must be provided for temporary bans.")
+    )
 
-        if any(
-                not i
-                for i in [
-                    staff_id,
-                    staff_name,
-                    user_id,
-                    user_name,
-                    guild_id,
-                    reason,
-                    moderation_type,
-                ]
-        ):
-            return ValueError("All arguments must be provided.")
+    try:
+        url_var = config("BASE_API_URL")
+        panel_url_var = config("PANEL_API_URL")
+        if url_var not in ["", None]:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                        f"{url_var}/Internal/SyncCreatePunishment/{identifier}", headers={
+                            "Authorization": config('INTERNAL_API_AUTH')
+                        }):
+                    pass
+        if panel_url_var not in ["", None]:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                        f"{panel_url_var}/{guild_id}/SyncCreatePunishment?ID={identifier}", headers={
+                            "X-Static-Token": config('PANEL_STATIC_AUTH')
+                        }):
+                    pass
+    except:
+        pass
 
-        identifier = ObjectId()
-
-        await self.db.insert_one(
-            {
-                "_id": identifier,
-                "Snowflake": next(generator),
-                "Username": user_name,
-                "UserID": user_id,
-                "Type": moderation_type,
-                "Reason": reason,
-                "Moderator": staff_name,
-                "ModeratorID": staff_id,
-                "Guild": guild_id,
-                "Epoch": int(time_epoch),
-                "UntilEpoch": int(until_epoch if until_epoch is not None else 0),
-            }
-        )
-
-        try:
-            url_var = config("BASE_API_URL")
-            if url_var not in ["", None]:
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(
-                            f"{url_var}/Internal/SyncCreatePunishment/{identifier}", headers={
-                                "Authorization": config('INTERNAL_API_AUTH')
-                            }):
-                        pass
-        except:
-            pass
-
-        return identifier
+    return identifier
 
     async def find_warning_by_spec(
             self,
